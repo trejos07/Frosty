0373ab1d1ff6334b6a5bb1872c2df2cb99d5df5a
Santiago Trejos <trejitos07@hotmail.com>
2019-08-16 18:00:56 -0500

    Player advance

---
+++ Server/Clases/Player.js
@@ -0,0 +1,9 @@
+var shortID = require('shortid');
+
+module.exports = class Player{
+    constructor(){
+        this.username ='';
+        this.id = shortID.generate();
+    }
+
+}
--- Server/index.js
+++ Server/index.js
@@ -1,12 +1,19 @@
 var io = require('socket.io')(process.env.PORT || 52300);
 
+//customClasses
+var Player = require('./Clases/Player.js');
+
 console.log('Server has Started');
+
+var players=[];
 
 io.on('connection', function(socket){
     console.log('connection Made :D');
 
     socket.on('disconnect', function (){
         console.log('A Player has disconnected');
+
+    
 
     });
 
