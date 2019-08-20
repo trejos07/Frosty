74322e58cb65f2ec15d22707255885abaac75b92
Santiago Trejos <trejitos07@hotmail.com>
2019-08-20 14:34:37 -0500

    position fix advance
    and some shit of unity

--- Unity_Project/Assets/Scripts/Networking/Position.cs
+++ Unity_Project/Assets/Scripts/Networking/Position.cs
@@ -1,14 +1,32 @@
 ﻿using System;
-
+using UnityEngine;
 
 namespace Frosty.Networking
 {
     [Serializable]
-    public class Position
+    public class Position : ISerializationCallbackReceiver
     {
         public float x;
         public float y;
         public float z;
+
+        public string _x;
+        public string _y;
+        public string _z;
+
+        public void OnAfterDeserialize()
+        {
+            x = float.Parse(_x);
+            y = float.Parse(_y);
+            z = float.Parse(_z);
+        }
+
+        public void OnBeforeSerialize()
+        {
+            _x = x.ToString();
+            _y = y.ToString();
+            _z = z.ToString();
+        }
 
         public override string ToString()
         {
